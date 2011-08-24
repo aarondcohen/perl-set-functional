11f214cbcf7ec6f79b964f355e0cfba76a9621d7
aaron cohen <aaron@aarondcohen.(none)>
2011-08-24 02:52:33 -0400

    Initialize repo

---
+++ lib/Set/Functional.pm
@@ -0,0 +1,111 @@
+package Set::Functional;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Set::Functional - The great new Set::Functional!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Set::Functional;
+
+    my $foo = Set::Functional->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Aaron Cohen, C<< <aarondcohen at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-set-functional at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Set-Functional>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Set::Functional
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Set-Functional>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Set-Functional>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Set-Functional>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Set-Functional/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Aaron Cohen.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Set::Functional
---
+++ t/00-load.t
@@ -0,0 +1,9 @@
+#!perl -T
+
+use Test::More tests => 1;
+
+BEGIN {
+    use_ok( 'Set::Functional' ) || print "Bail out!\n";
+}
+
+diag( "Testing Set::Functional $Set::Functional::VERSION, Perl $], $^X" );
